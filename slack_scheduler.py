1a386c6bc9fb26e45cb2139e1d30e0244625c41c
Pependo85 <armando@dashboardmx.com>
2024-08-06 16:33:40 -0600

    Update slack_scheduler.py
    Scrip Mejorado

--- slack_scheduler.py
+++ slack_scheduler.py
@@ -3,8 +3,16 @@ import time
 from datetime import datetime
 import os
 
+# Obtener los secrets desde las variables de entorno
 SLACK_TOKEN = os.getenv('SLACK_TOKEN')
 USER_GROUP_ID = os.getenv('USER_GROUP_ID')
+
+# Verificar que los secrets están configurados correctamente
+if not SLACK_TOKEN or not USER_GROUP_ID:
+    raise ValueError("SLACK_TOKEN or USER_GROUP_ID not set in the environment")
+
+print(f"SLACK_TOKEN: {SLACK_TOKEN}")
+print(f"USER_GROUP_ID: {USER_GROUP_ID}")
 
 # Definición de los IDs de los usuarios
 users = {
@@ -53,6 +61,9 @@ def update_user_group(user_id):
         'users': user_id,
     }
     response = requests.post(url, headers=headers, json=data)
+    print(f"Request to {url} with data {data}")
+    if response.status_code != 200:
+        raise Exception(f"Error updating user group: {response.status_code} - {response.text}")
     print(f"Updated user group with user ID {user_id}: {response.json()}")
     return response.json()
 
@@ -63,17 +74,23 @@ def get_current_day():
     return datetime.now().strftime('%A')
 
 def run_scheduler():
-    current_hour = get_current_hour()
-    current_day = get_current_day()
+    while True:
+        current_hour = get_current_hour()
+        current_day = get_current_day()
 
-    if current_day in ['Monday', 'Tuesday', 'Wednesday', 'Thursday']:
-        if current_hour in horarios_lunes_jueves:
-            print(f"Updating user group at {current_hour} on {current_day}")
-            update_user_group(horarios_lunes_jueves[current_hour])
-    elif current_day == 'Friday':
-        if current_hour in horarios_viernes:
-            print(f"Updating user group at {current_hour} on {current_day}")
-            update_user_group(horarios_viernes[current_hour])
+        print(f"Current time: {current_hour} on {current_day}")
+
+        if current_day in ['Monday', 'Tuesday', 'Wednesday', 'Thursday']:
+            if current_hour in horarios_lunes_jueves:
+                print(f"Updating user group at {current_hour} on {current_day}")
+                update_user_group(horarios_lunes_jueves[current_hour])
+        elif current_day == 'Friday':
+            if current_hour in horarios_viernes:
+                print(f"Updating user group at {current_hour} on {current_day}")
+                update_user_group(horarios_viernes[current_hour])
+
+        # Esperar un minuto antes de verificar nuevamente
+        time.sleep(60)
 
 if __name__ == '__main__':
     run_scheduler()
